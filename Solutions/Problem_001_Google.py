3f7a75fef1c2de12abdb8328d179c0c13761a67f
Matheus <matheus.depauladomingos@gmail.com>
2020-07-27 12:56:13 -0300

    Update Problem_001_Google.py

--- Solutions/Problem_001_Google.py
+++ Solutions/Problem_001_Google.py
@@ -5,7 +5,7 @@ For example, given [10, 15, 3, 7] and k of 17,
 return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass? """
 
-def verify_num(nList, k):
+def verify_num(n_List, k):
     for num_1 in n_List:
         for num_2 in n_List:
             if num_1 + num_2 == k:
